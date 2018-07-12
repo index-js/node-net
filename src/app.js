7f84fe139150029cbc19450f1e24e8f311fbf650
Yanglin <i@yangl.in>
2018-07-12 17:09:48 +0800

    indent onerror bind context

--- src/app.js
+++ src/app.js
@@ -10,126 +10,123 @@ const request = require('./req')
 const response = require('./res')
 
 class Application extends EventEmitter{
-    constructor() {
-        super()
-        this.middlewares = []
-        this.callbacks = []
-        this.context = Object.create(context)
-        this.request = Object.create(request)
-        this.response = Object.create(response)
-    }
+  constructor() {
+    super()
+    this.middlewares = []
+    this.callbacks = []
+    this.context = Object.create(context)
+    this.request = Object.create(request)
+    this.response = Object.create(response)
+  }
 
-    onerror(err) {
-        console.error('err:', err)
-    }
+  onerror(err) {
+    console.error('err:', err)
+  }
 
-    createContext(req, res) {
-        const context = Object.create(this.context)
+  createContext(req, res) {
+    const context = Object.create(this.context)
 
-        let request = context.request = Object.create(this.request)
-        let response = context.response = Object.create(this.response)
+    let request = context.request = Object.create(this.request)
+    let response = context.response = Object.create(this.response)
 
-        context.app = request.app = response.app = this
-        context.req = request.req = response.req = req
-        context.res = request.res = response.res = res
+    context.app = request.app = response.app = this
+    context.req = request.req = response.req = req
+    context.res = request.res = response.res = res
 
-        context.cookies = new cookies(req, res, {
-            key: this.keys,
-            secure: request.secure
-        })
-        context.state = {}
+    context.cookies = new cookies(req, res, {
+      key: this.keys,
+      secure: request.secure
+    })
+    context.state = {}
 
-        return context
-    }
+    return context
+  }
 
-    callback() {
-        if (!this.listenerCount('error')) this.on('error', this.onerror)
+  callback() {
+    if (!this.listenerCount('error')) this.on('error', this.onerror)
 
-        return (req, res) => {
-            let index = -1
-            let range = 0
+    return (req, res) => {
+      let index = -1
+      let range = 0
 
-            const next = async() => {
-                const fn = this.middlewares[++ index]
-                try {
-                    if(fn) {
-                        await fn.call(ctx, next)
+      const next = async() => {
+        const fn = this.middlewares[++ index]
+        try {
+          if(fn) {
+            await fn.call(ctx, next)
 
-                        if (++ range != this.middlewares.length) next()  // Without using next, auto append
-                        else Promise.all(this.callbacks.map(fn => fn.call(ctx))).then(respond.call(ctx))
-                    }
-                }
-                catch (e) {
-                    ctx.status = 500
-                    ctx.body = 'error: ' + String(e)
-                    respond.call(ctx)
-                    this.emit('error', e)
-                }
-            }
-
-            const ctx = this.createContext(req, res)
-            ctx.status = 404
-            
-            return next()
+            if (++ range < this.middlewares.length) next()  // Without using next, auto append
+            else Promise.all(this.callbacks.map(fn => fn.call(ctx))).then(respond.call(ctx))
+          }
         }
-    }
-
-    listen(...args) {
-        const server = http.createServer(this.callback())
-        return server.listen(...args)
-    }
-
-    use(fn, cb) {
-        if (typeof fn !== 'function') throw new Error('Middleware must be a function!')
-        if (isArrow(fn)) throw new Error('Middleware can not use arrow function!')
-        this.middlewares.push(fn)
-
-        if (cb){
-            if (typeof fn !== 'function') throw new Error('Callback must be a function!')
-            if (isArrow(cb)) throw new Error('Callback can not use arrow function!')
-            this.callbacks.unshift(cb)
+        catch (e) {
+          ctx.onerror(e)
         }
+      }
 
-        return this
+      const ctx = this.createContext(req, res)
+      ctx.status = 404
+      
+      return next()
     }
+  }
+
+  listen(...args) {
+    const server = http.createServer(this.callback())
+    return server.listen(...args)
+  }
+
+  use(fn, cb) {
+    if (typeof fn !== 'function') throw new Error('Middleware must be a function!')
+    if (isArrow(fn)) throw new Error('Middleware can not use arrow function!')
+    this.middlewares.push(fn)
+
+    if (cb){
+      if (typeof fn !== 'function') throw new Error('Callback must be a function!')
+      if (isArrow(cb)) throw new Error('Callback can not use arrow function!')
+      this.callbacks.unshift(cb)
+    }
+
+    return this
+  }
 }
 
 function isArrow(functionObject) {
-    return /^(async *)?\(/.test(functionObject)
+  return /^(async *)?\(/.test(functionObject)
 }
 
 function respond() {
-    const res = this.res
-    // if (!res.writable) return
+  const res = this.res
+  // if (!res.writable) return
 
-    let body = this.body
-    let code = this.status
+  let body = this.body
+  let code = this.status
 
-    if (statuses.empty[code]) {
-        this.body = null
-        return res.end()
-    }
+  if (statuses.empty[code]) {
+    this.body = null
+    return res.end()
+  }
 
-    if (Buffer.isBuffer(body)) return res.end(body)
+  if (Buffer.isBuffer(body)) return res.end(body)
 
-    if ('HEAD' === this.method) {
-        this.length = String(body).length
-        return res.end()
-    }
+  if ('HEAD' === this.method) {
+    this.length = String(body).length
+    return res.end()
+  }
 
-    if (null == body) {
-        body = this.message || String(code)
-        this.type = 'text'
-        this.length = Buffer.byteLength(body)
-        return res.end(body)
-    }
-
-    if ('string' === typeof body) return res.end(body)
-    if (body instanceof Stream) return body.pipe(res)
-
-    body = JSON.stringify(body)
+  if (null == body) {
+    body = this.message || String(code)
+    this.type = 'text'
     this.length = Buffer.byteLength(body)
-    res.end(body)
+    return res.end(body)
+  }
+
+  if ('string' === typeof body) return res.end(body)
+  if (body instanceof Stream) return body.pipe(res)
+
+  body = JSON.stringify(body)
+  this.length = Buffer.byteLength(body)
+  res.end(body)
 }
 
 module.exports = Application
--- src/req.js
+++ src/req.js
@@ -4,51 +4,51 @@ const url = require('url')
 const util = require('util')
 
 module.exports = {
-	get headers() {
-		return this.req.headers
-	},
-	get url() {
-		return this.path
-	},
-	get originalUrl() {
-		return this.req.originalUrl
-	},
-	get method() {
-		return this.req.method
-	},
-	get path() {
-		return url.parse(this.req.url, true).pathname
-	},
-	get href() {
-		return this.req.url
-	},
-	get query() {
-		return url.parse(this.req.url, true).query
-	},
-	get search() {
-		return url.parse(this.req.url, true).search
-	},
-	get socket() {
-		return this.req.socket
-	},
-	get protocol() {
-		if (this.socket.encrypted) return 'https'
-		
-		const proto = this.get('X-Forwarded-Proto')
-		return proto ? proto.split(/\s*,\s*/)[0] : 'http'
-	},
-	get secure() {
-		return 'https' == this.protocol
-	},
-	get(field) {
-		const headers = this.headers
+  get headers() {
+    return this.req.headers
+  },
+  get url() {
+    return this.path
+  },
+  get originalUrl() {
+    return this.req.originalUrl
+  },
+  get method() {
+    return this.req.method
+  },
+  get path() {
+    return url.parse(this.req.url, true).pathname
+  },
+  get href() {
+    return this.req.url
+  },
+  get query() {
+    return url.parse(this.req.url, true).query
+  },
+  get search() {
+    return url.parse(this.req.url, true).search
+  },
+  get socket() {
+    return this.req.socket
+  },
+  get protocol() {
+    if (this.socket.encrypted) return 'https'
+    
+    const proto = this.get('X-Forwarded-Proto')
+    return proto ? proto.split(/\s*,\s*/)[0] : 'http'
+  },
+  get secure() {
+    return 'https' == this.protocol
+  },
+  get(field) {
+    const headers = this.headers
 
-		switch (field = field.toLowerCase()) {
-			case 'referer':
-			case 'referrer':
-				return headers.referrer || headers.referer || ''
-			default:
-				return headers[field] || ''
-		}
-	}
+    switch (field = field.toLowerCase()) {
+      case 'referer':
+      case 'referrer':
+        return headers.referrer || headers.referer || ''
+      default:
+        return headers[field] || ''
+    }
+  }
 }
