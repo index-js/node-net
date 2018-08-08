67ffd2b052cc8f5c5266a6f0b5abc6d95eb5199f
Yanglin <i@yangl.in>
2018-08-08 18:34:25 +0800

    new think, less code less result

--- src/app.js
+++ src/app.js
@@ -1,41 +1,12 @@
-'use strict';
+'use strict'
 
 const EventEmitter = require('events')
 const http = require('http')
-const cookies = require('cookies')
-const statuses = require('statuses')
 
-const context = require('./ctx')
-const request = require('./req')
-const response = require('./res')
-
-class Application extends EventEmitter{
+module.exports = class Application extends EventEmitter {
   constructor() {
     super()
     this.middlewares = []
-    this.callbacks = []
-    this.context = Object.create(context)
-    this.request = Object.create(request)
-    this.response = Object.create(response)
-  }
-
-  createContext(req, res) {
-    const context = Object.create(this.context)
-
-    let request = context.request = Object.create(this.request)
-    let response = context.response = Object.create(this.response)
-
-    context.app = request.app = response.app = this
-    context.req = request.req = response.req = req
-    context.res = request.res = response.res = res
-
-    context.cookies = new cookies(req, res, {
-      key: this.keys,
-      secure: request.secure
-    })
-    context.state = {}
-
-    return context
   }
 
   callback() {
@@ -45,88 +16,31 @@ class Application extends EventEmitter{
       let index = -1
       let range = 0
 
-      const next = async () => {
+      const next = async i => {
+        if (i <= index) return Promise.reject('next() called multiple times')
+
         const fn = this.middlewares[++ index]
         if (!fn) return Promise.resolve()
 
         try {
-          await fn.apply(ctx, [next, ctx])
-
+          await fn(req, res, next.bind(null, i + 1))
           if (++ range < this.middlewares.length) next()  // Without using next, auto append
-          else Promise.all(this.callbacks.map(fn => fn.call(ctx, ctx))).then(respond.call(ctx, ctx))
-        }
-        catch (e) {
-          ctx.onerror(e)
+        } catch (e) {
+          this.emit('error', e)
         }
       }
 
-      const ctx = this.createContext(req, res)
-      ctx.status = 404
-      
-      return next()
+      return next(0)
     }
+  }
+
+  use(fn) {
+    if (typeof fn !== 'function') throw new Error('Middleware must be a function!')
+    this.middlewares.push(fn)
   }
 
   listen(...args) {
     const server = http.createServer(this.callback())
     return server.listen(...args)
   }
-
-  use(fn, cb) {
-    if (typeof fn !== 'function') throw new Error('Middleware must be a function!')
-    if (isArrow(fn)) console.warn('\x1B[31m%s\x1B[39m\x1B[33m%s\x1B[39m\x1B[32m%s\x1B[39m',
-      'Middleware has used arrow function, ','please notice "this" keywords,\n',
-      'or you can use "context" after the "next" parameter')
-    this.middlewares.push(fn)
-
-    if (cb){
-      if (typeof fn !== 'function') throw new Error('Callback must be a function!')
-      if (isArrow(cb)) console.warn('\x1B[31m%s\x1B[39m\x1B[33m%s\x1B[39m\x1B[32m%s\x1B[39m',
-        'Callback has used arrow function, ','please notice "this" keywords,\n',
-        'or you can use "context" after the "next" parameter')
-      this.callbacks.unshift(cb)
-    }
-
-    return this
-  }
 }
-
-function isArrow(functionObject) {
-  return /^(async *)?\(/.test(functionObject)
-}
-
-function respond() {
-  const res = this.res
-  // if (!res.writable) return
-
-  let body = this.body
-  let code = this.status
-
-  if (statuses.empty[code]) {
-    this.body = null
-    return res.end()
-  }
-
-  if (Buffer.isBuffer(body)) return res.end(body)
-
-  if ('HEAD' === this.method) {
-    this.length = String(body).length
-    return res.end()
-  }
-
-  if (null == body) {
-    body = this.message || String(code)
-    this.type = 'text'
-    this.length = Buffer.byteLength(body)
-    return res.end(body)
-  }
-
-  if ('string' === typeof body) return res.end(body)
-  if (body instanceof Stream) return body.pipe(res)
-
-  body = JSON.stringify(body)
-  this.length = Buffer.byteLength(body)
-  res.end(body)
-}
-
-module.exports = Application
--- test.js
+++ test.js
@@ -2,46 +2,22 @@ const server = require('./index')
 const app = new server()
 const assert = require('assert')
 
-// m1
-app.use(async function (next) {
-    console.log(this.url, 'm1')
-    this.cookies.set('55', 555)
-    setTimeout(() => {
-        // assert.equal(this, app, 'this and app should equal')
-    })
-    await next()
-    console.log('m1 await then')
-}, function () {
-    console.log('m1 end\n')
+app.use(async function(req, res, next) {
+  res.write('content')
+  await next()
+  console.log('back end')
 })
 
-// m2
-app.use(function (next) {
-    // throw 'm2 err'
-    console.log('m2')
-    this.cookies.get('55')
-    this.body = '<body> - ' + Date.now()
-    this.status = 200
-    next()
-}, function (req, res) {
-    console.log('m2 callback')
+app.use(function(req, res, next) {
+  res.write('middle')
 })
 
-// m3
-app.use(async function (next) {
-    console.log('m3')
-    // await next()
-    console.log('m3-end')
-}, function() {
-    console.log('m3 callback begin')
-})
-
-app.keys = '12345'
-app.on('error', (err) => {
-    console.log('err: ' + err)
-    // throw new Error(err)
+app.use(async function(req, res, next) {
+  console.log('back start')
+  await next()
+  res.end('end')
 })
 
 app.listen(3000, () => {
-    console.log('listening on 3000')
+  console.log('127.0.0.1:3000')
 })
